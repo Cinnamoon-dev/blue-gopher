c45f063bd732091240f97497a11168910e4c1d22
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2026-02-03 08:31:25 -0300

    refactor: login fields added to user repository

--- repositories/userRepository.go
+++ repositories/userRepository.go
@@ -12,9 +12,9 @@ type UserRepository struct {
 }
 
 type User struct {
-	ID    int    `json:"id"`
-	Nome  string `json:"nome"`
-	Idade int    `json:"idade"`
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 func NewUserRepository(db *sql.DB) UserRepository {
@@ -22,7 +22,7 @@ func NewUserRepository(db *sql.DB) UserRepository {
 }
 
 func (r *UserRepository) GetAll() ([]User, error) {
-	rows, err := r.db.Query("SELECT id, nome, idade FROM usuarios;")
+	rows, err := r.db.Query("SELECT id, email, password FROM usuarios ORDER BY id;")
 	if err != nil {
 		return nil, &errors.HTTPError{Message: "Database error", Status: http.StatusInternalServerError}
 	}
@@ -32,7 +32,7 @@ func (r *UserRepository) GetAll() ([]User, error) {
 	var user User
 
 	for rows.Next() {
-		err := rows.Scan(&user.ID, &user.Nome, &user.Idade)
+		err := rows.Scan(&user.ID, &user.Email, &user.Password)
 		if err != nil {
 			return nil, &errors.HTTPError{Message: "Database error", Status: http.StatusInternalServerError}
 		}
@@ -46,18 +46,18 @@ func (r *UserRepository) GetAll() ([]User, error) {
 func (r *UserRepository) Get(id int) (*User, error) {
 	var user User
 
-	row := r.db.QueryRow("SELECT id, nome, idade FROM usuarios WHERE id = ?", id)
-	if err := row.Scan(&user.ID, &user.Nome, &user.Idade); err != nil {
+	row := r.db.QueryRow("SELECT id, email, password FROM usuarios WHERE id = ? ORDER BY id", id)
+	if err := row.Scan(&user.ID, &user.Email, &user.Password); err != nil {
 		return nil, &errors.HTTPError{Message: "Not found", Status: http.StatusNotFound}
 	}
 
 	return &user, nil
 }
 
-func (r *UserRepository) GetByName(name string) (*User, error) {
+func (r *UserRepository) GetByEmail(name string) (*User, error) {
 	var user User
-	row := r.db.QueryRow("SELECT id, nome, idade FROM usuarios WHERE nome = ?", name)
-	if err := row.Scan(&user.ID, &user.Nome, &user.Idade); err != nil {
+	row := r.db.QueryRow("SELECT id, email, password FROM usuarios WHERE email = ? ORDER BY id LIMIT 1", name)
+	if err := row.Scan(&user.ID, &user.Email, &user.Password); err != nil {
 		return nil, &errors.HTTPError{Status: http.StatusNotFound, Message: "Not found"}
 	}
 
@@ -65,7 +65,7 @@ func (r *UserRepository) GetByName(name string) (*User, error) {
 }
 
 func (r *UserRepository) Create(user User) (int64, error) {
-	result, err := r.db.Exec("INSERT INTO usuarios(nome, idade) VALUES (?, ?)", user.Nome, user.Idade)
+	result, err := r.db.Exec("INSERT INTO usuarios(email, password) VALUES (?, ?)", user.Email, user.Password)
 	if err != nil {
 		return 0, &errors.HTTPError{Message: "Database error", Status: http.StatusInternalServerError}
 	}
@@ -79,7 +79,7 @@ func (r *UserRepository) Create(user User) (int64, error) {
 }
 
 func (r *UserRepository) Update(id int, user User) error {
-	_, err := r.db.Exec("UPDATE usuarios SET nome = ?, idade = ? WHERE id = ?", user.Nome, user.Idade, id)
+	_, err := r.db.Exec("UPDATE usuarios SET email = ?, password = ? WHERE id = ?", user.Email, user.Password, id)
 	if err != nil {
 		return &errors.HTTPError{Message: "Database error", Status: http.StatusInternalServerError}
 	}
@@ -93,5 +93,5 @@ func (r *UserRepository) Delete(id int) error {
 		return &errors.HTTPError{Message: "Database error", Status: http.StatusInternalServerError}
 	}
 
-	return err
+	return nil
 }
