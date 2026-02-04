db5d28f75f4ea201665b4f90add18db42fbe7370
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2026-02-04 09:20:41 -0300

    feat: user domain created

---
+++ internal/domain/user.go
@@ -0,0 +1,41 @@
+package domain
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+type User struct {
+	ID       int
+	Email    string
+	Password string
+	RoleID   int
+}
+
+func (u *User) ValidateEmail() error {
+	email := u.Email
+	email = strings.TrimSpace(email)
+
+	if email == "" {
+		return errors.New("Validate email: email is required")
+	}
+
+	emailRegex := `^[a-zA-Z0-9.!#$%&'*+/=?^_` + "`" + `{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`
+	re := regexp.MustCompile(emailRegex)
+
+	if re.MatchString(email) {
+		return nil
+	}
+
+	return fmt.Errorf("Validate email: invalid email %s", email)
+}
+
+func (u *User) ValidatePassword() error {
+	if len(u.Password) < 4 {
+		return errors.New("Validate password: password is too short (minimum 4 characters)")
+	}
+
+	return nil
+}
