4e735b58a36e59fcba233608acc03ceead53090d
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2026-02-04 10:57:43 -0300

    refactor: role repository added to user service

--- internal/services/userService.go
+++ internal/services/userService.go
@@ -9,42 +9,48 @@ import (
 )
 
 type UserService struct {
-	Repo repositories.UserRepository
+	UserRepo repositories.UserRepository
+	RoleRepo repositories.RoleRepository
 }
 
-func NewUserService(Repo repositories.UserRepository) UserService {
-	return UserService{Repo: Repo}
+func NewUserService(userRepo repositories.UserRepository, roleRepo repositories.RoleRepository) UserService {
+	return UserService{UserRepo: userRepo, RoleRepo: roleRepo}
 }
 
 func (s *UserService) GetAll() ([]domain.User, error) {
-	return s.Repo.GetAll()
+	return s.UserRepo.GetAll()
 }
 
 func (s *UserService) Get(id int) (*domain.User, error) {
-	return s.Repo.Get(id)
+	return s.UserRepo.Get(id)
 }
 
 func (s *UserService) GetByEmail(email string) (*domain.User, error) {
-	return s.Repo.GetByEmail(email)
+	return s.UserRepo.GetByEmail(email)
 }
 
 func (s *UserService) Create(newUser domain.User) (int64, error) {
-	_, err := s.Repo.GetByEmail(newUser.Email)
+	_, err := s.UserRepo.GetByEmail(newUser.Email)
 	if err == nil {
 		return 0, &errors.HTTPError{Status: 422, Message: fmt.Sprintf("Email %s already in use", newUser.Email)}
 	}
 
-	return s.Repo.Create(newUser)
+	_, err = s.RoleRepo.Get(newUser.RoleID)
+	if err == nil {
+		return 0, &errors.HTTPError{Status: 422, Message: fmt.Sprintf("Role with id %d does not exists", newUser.RoleID)}
+	}
+
+	return s.UserRepo.Create(newUser)
 }
 
 func (s *UserService) Update(id int, fields domain.User) error {
-	if _, err := s.Repo.Get(id); err != nil {
+	if _, err := s.UserRepo.Get(id); err != nil {
 		return &errors.HTTPError{Status: 404, Message: fmt.Sprintf("User %d not found", id)}
 	}
 
-	return s.Repo.Update(id, fields)
+	return s.UserRepo.Update(id, fields)
 }
 
 func (s *UserService) Delete(id int) error {
-	return s.Repo.Delete(id)
+	return s.UserRepo.Delete(id)
 }
