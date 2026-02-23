67bd2b63590da44ad4feecd715fd8eae37064d77
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2026-02-23 10:40:07 -0300

    feat: role validation added

--- internal/domain/role.go
+++ internal/domain/role.go
@@ -1,6 +1,21 @@
 package domain
 
+import (
+	"errors"
+	"strings"
+)
+
 type Role struct {
 	ID   int64
 	Name string
+}
+
+func (r *Role) ValidateName() error {
+	r.Name = strings.TrimSpace(r.Name)
+
+	if r.Name == "" {
+		return errors.New("Validate name: name is required")
+	}
+
+	return nil
 }
