b40c9ac578ff020aec358c63296482969979595b
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2026-03-16 09:26:56 -0300

    refactor: mail router now uses mail handler

--- internal/http/routers/mailRouter.go
+++ internal/http/routers/mailRouter.go
@@ -2,68 +2,28 @@ package routers
 
 import (
 	"net/http"
-	"strings"
 
 	"github.com/Cinnamoon-dev/blue-gopher/internal/http/handlers"
 	"github.com/Cinnamoon-dev/blue-gopher/internal/middleware"
-	"github.com/Cinnamoon-dev/blue-gopher/internal/services"
-	"github.com/Cinnamoon-dev/blue-gopher/pkg/config"
-	"github.com/golang-jwt/jwt/v5"
 )
 
 type MailRouter struct {
-	AuthService services.AuthService
-	UserService services.UserService
+	MailHandler handlers.MailHandler
 }
 
-func NewMailRouter(authService services.AuthService, userService services.UserService) MailRouter {
+func NewMailRouter(mailHandler handlers.MailHandler) MailRouter {
 	return MailRouter{
-		AuthService: authService,
-		UserService: userService,
+		MailHandler: mailHandler,
 	}
-}
-
-func parseToken(path string) (string, error) {
-	// path = /mail/{token}
-	parts := strings.Split(path, "/")
-
-	if len(parts) < 3 {
-		return "", http.ErrNotSupported
-	}
-
-	return parts[2], nil
 }
 
 func (ro *MailRouter) BaseRoutes() http.HandlerFunc {
 	router := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-
 		switch r.Method {
 		case http.MethodGet:
-			env := config.NewEnv()
-			emailToken, err := parseToken(r.URL.Path)
-			if err != nil {
-				handlers.RespondError(w, err)
-				return
-			}
-
-			claims, err := ro.AuthService.DecodeMailToken(emailToken, jwt.SigningMethodHS256, []byte(env.JwtKey))
-			if err != nil {
-				handlers.RespondError(w, err)
-				return
-			}
-
-			email := claims.Email
-			user, err := ro.UserService.GetByEmail(ctx, email)
-			if err != nil {
-				handlers.RespondError(w, err)
-				return
-			}
-
-			handlers.RespondJSON(w, http.StatusOK, user)
-			return
-			// Adicionar a coluna isverified a user table e mexer com todos os metodos de user repository
-			// adicionar o datetime atual ao is_verified se for nulo se ja for verificado, não fazer nada e dizer que ja esta verificado
+			ro.MailHandler.VerifyUser(w, r)
+		case http.MethodPost:
+			ro.MailHandler.SendEmail(w, r)
 		}
 	})
 
