0c18cf650a5e2b029368cf3ae25f2c67b73585d3
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2026-03-18 11:37:57 -0300

    feat: publisher added to user service

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Cinnamoon-dev/blue-gopher/internal/database"
 	"github.com/Cinnamoon-dev/blue-gopher/internal/http/handlers"
 	"github.com/Cinnamoon-dev/blue-gopher/internal/http/routers"
+	"github.com/Cinnamoon-dev/blue-gopher/internal/messaging/rabbitmq"
 	"github.com/Cinnamoon-dev/blue-gopher/internal/repositories"
 	"github.com/Cinnamoon-dev/blue-gopher/internal/services"
 	"github.com/Cinnamoon-dev/blue-gopher/pkg/config"
@@ -22,15 +23,23 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
+	db.Exec("PRAGMA jounal_mode=WAL;")
 
 	database.CreateTables("./internal/database/tables.sql", db)
 	database.Populate("./internal/database/rules.sql", db)
 	database.RunAllMigrations(db)
 
+	queueConn, err := rabbitmq.NewConnection(env.RabbitMQUrl)
+	if err != nil {
+		log.Panicf("Couldn't connect to RabbitMQ: %s", err)
+	}
+
+	publisher := rabbitmq.NewRabbitPublisher(*queueConn)
+
 	roleRepository := repositories.NewRoleRepository(db)
 
 	userRepository := repositories.NewUserRepository(db)
-	userService := services.NewUserService(userRepository, roleRepository)
+	userService := services.NewUserService(userRepository, roleRepository, *publisher)
 	userHandler := handlers.NewUserHandler(userService)
 	userRouter := routers.NewUserRouter(userHandler)
 
